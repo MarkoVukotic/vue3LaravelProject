c02d5473da4e795a584424210e12cc17cd9f4310
marko.v <markovukotic32@gmail.com>
2022-10-23 12:45:08 +0200

    Load table data with Vue.js 3 Composition API

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,16 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
-
+const colors = require('tailwindcss/colors')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
     ],
 
     theme: {
+        colors,
         extend: {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
