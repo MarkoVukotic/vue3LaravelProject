782e2bc6eeca4d6a426169744b09d165f9732212
marko.v <markovukotic32@gmail.com>
2022-10-24 12:50:12 +0200

    Adding Pagination and PostResource

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,17 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
 use App\Models\Post;
-use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index(){
         try {
 
-            return Post::all();
+            return PostResource::collection(Post::paginate(10));
 
-        }catch (\Exception $exception){
+        } catch (\Exception $exception){
             echo $exception->getLine();
             echo $exception->getMessage();
         }
--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,9 @@ import './bootstrap';
 import { createApp } from 'vue/dist/vue.esm-bundler';
 import PostsIndex from './components/Posts/Index.vue'
 
+import LaravelVuePagination from "laravel-vue-pagination";
+
 const app = createApp({})
 app.component('posts-index', PostsIndex)
+app.component('Pagination' , LaravelVuePagination)
 app.mount('#app')
